f00dfa05e7dc5c9b691352610fbbf19fd733e57a
janaalg <janatariq143@gmail.com>
2022-03-02 19:47:13 +0300

    added my name

---
+++ jana/jana/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jana
+//
+//  Created by Daughter Jana on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello my name is jana")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jana/jana/janaApp.swift
@@ -0,0 +1,17 @@
+//
+//  janaApp.swift
+//  jana
+//
+//  Created by Daughter Jana on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct janaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
